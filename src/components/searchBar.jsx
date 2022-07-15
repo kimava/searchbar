cc429e4e49f6cb8bfbd124739c4b71205355f533
Ava <ava.soyeon.kim@gmail.com>
2022-07-15 13:35:08 +0900

    reflect autocomplete to input and add key event for searching via input

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchBar({ setQuery, suggestion, link }) {
+  const [completedWord, setCompletedWord] = useState('');
+
+  function onSubmit(event) {
+    if (event.key === 'Enter' && completedWord.length > 0) {
+      window.open(link(completedWord), '_blank');
+    }
+  }
+
   return (
     <div>
       <input
         placeholder='검색어를 입력하세요'
+        value={completedWord}
         onChange={(e) => {
+          setCompletedWord(e.currentTarget.value);
           setQuery(e.currentTarget.value);
         }}
+        onKeyPress={onSubmit}
       />
       {suggestion && (
         <ul>
           {suggestion.map((item) => (
             <li key={item}>
-              <a target='_blank' href={link(item)}>
+              <a
+                target='_blank'
+                onClick={() => {
+                  setCompletedWord(item);
+                }}
+                href={link(item)}
+              >
                 {item}
               </a>
             </li>
